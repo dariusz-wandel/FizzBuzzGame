6cd88435e8d13cb2295d8672c966754b4e9c1767
Dariusz Wandel <dariusz.wandel@gmail.com>
2023-02-14 22:54:13 +0100

    Add project files

---
+++ FizzBuzzGame/FizzBuzz.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+    internal class FizzBuzz
+    {
+        public string PlayFizzBuzz(int number)
+        {
+            string output = "";
+                        {
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    output += "FizzBuzz\n";
+                }
+                else if (number % 3 == 0)
+                {
+                    output += "Fizz\n";
+                }
+                else if (number % 5 == 0)
+                {
+                    output += "Buzz\n";
+                }
+                else
+                {
+                    output += number.ToString() + "\n";
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ FizzBuzzGame/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new FizzBuzz();
+            bool playAgain = true;
+            
+            while (playAgain)
+            {
+                Console.WriteLine("Wprowadź liczbę, aby zagrać w grę FizzBuzz:");
+                var number = GetNumber();
+                Console.WriteLine(game.PlayFizzBuzz(number));
+                Console.WriteLine($"Gramy jeszcze raz? (T/N)");
+                string answer = Console.ReadLine();
+                if (answer.ToLower() != "t")
+                {
+                    playAgain= false;
+                }
+            }
+        }
+
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Wprowadzona wartość nie jest prawidłową liczbą. Spróbuj jeszcze raz.");
+                    continue;
+                }
+                return number;
+            }
+        }
+    }
+}
